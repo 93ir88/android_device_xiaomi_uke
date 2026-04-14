69165f781ebf9b62075922bb4a36abfa7adee9da
Sebastiano Barezzi <seba@sebaubuntu.dev>
2026-04-14 21:46:17 +0200

    uke: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: arm64 Manufacturer: xiaomi Model: 2410CRP4CG Script version: 3.0.0

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2026 The Android Open Source Project
+# Copyright (C) 2026 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_uke.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_uke-user \
+    omni_uke-userdebug \
+    omni_uke-eng
